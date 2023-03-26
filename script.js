7bc47b232dbe203ec14e6206ff564735ad867d4c
Arcerr <taurus..ali47>
2023-03-27 00:04:02 +0300

    .

--- script.js
+++ script.js
@@ -4,3 +4,48 @@ $('.phone-menu').on('click', function () {
    $('.phone-style').toggleClass('p-active');
    $('i.fa-bars').toggleClass('fa-beat');
 });
+
+
+
+// Show elements while scrolling down
+function Utils() {}
+        Utils.prototype = {
+            constructor: Utils,
+            isElementInView: function (element, fullyInView) {
+                var pageTop = $(window).scrollTop();
+                var pageBottom = pageTop + $(window).height();
+                var elementTop = $(element).offset().top;
+                var elementBottom = elementTop + $(element).height();
+
+                if (fullyInView === true) {
+                    return ((pageTop < elementTop) && (pageBottom > elementBottom));
+                } else {
+                    return ((elementTop <= pageBottom) && (elementBottom >= pageTop));
+                }
+            }
+        };
+
+        var Utils = new Utils();
+        $(window).on('load', addFadeIn());
+        
+        $(window).scroll(function() {
+            addFadeIn(true);
+        });
+
+function addFadeIn(repeat) {
+            // var classToFadeIn = ".services-whatcando-ul > li";
+            var classToFadeIn = document.querySelectorAll('.will-fadeIn')
+            
+            $(classToFadeIn).each(function( index ) {
+                var isElementInView = Utils.isElementInView($(this), false);
+                if (isElementInView) {
+                    if(!($(this).hasClass('fadeInRight')) && !($(this).hasClass('fadeInLeft'))) {
+                        if(index % 2 == 0) $(this).addClass('fadeInRight');
+                        else $(this).addClass('fadeInLeft');
+                    }
+                } else if(repeat) {
+                    $(this).removeClass('fadeInRight');
+                    $(this).removeClass('fadeInLeft');
+                }
+            });
+        }
